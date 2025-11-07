625c6359a5a8b80d2a68ea68497068e52ecd26e3
Kevin <zuluaga.kevin@correounivalle.edu.co>
2025-11-06 20:29:43 -0500

    implementacion de archivos para reserva y tipo de servicio

---
+++ src/modules/tipo servicio/tipoServicios.js
@@ -0,0 +1,38 @@
+
+const recogerRestaurante = document.getElementById('recoger-restaurante');
+const entregaDomicilio = document.getElementById('entrega-domicilio');
+const direccionEntrega = document.getElementById('direccion');
+const telefonoContacto = document.getElementById('telefono');
+
+
+
+
+
+recogerRestaurante.addEventListener('change', suspenderBotones);
+entregaDomicilio.addEventListener('change', suspenderBotones);
+
+
+function suspenderBotones() {
+    if (recogerRestaurante.checked) {
+
+        // Deshabilitar las otras dos opciones
+        entregaDomicilio.disabled = true;
+        // Desactivar los campos de contacto
+        direccionEntrega.disabled = true;
+        telefonoContacto.disabled = true;
+    }
+    else if (entregaDomicilio.checked) {
+        // Deshabilitar las otras dos opciones
+        recogerRestaurante.disabled = true;
+    }
+
+
+    else {
+        // Habilitar todas las opciones
+        entregaDomicilio.disabled = false;
+        recogerRestaurante.disabled = false;
+        // Habilitar los campos de contacto
+        direccionEntrega.disabled = false;
+        telefonoContacto.disabled = false;
+    }
+}
